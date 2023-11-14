2e19063ca9bd39896f90c50438151b7d17003833
Home54 <160515446@qq.com>
2023-11-14 23:12:17 +0800

    Restore the gun para in C# class

---
+++ Gun-Resources/DefaultGameConfig.cs
@@ -0,0 +1,13 @@
+namespace DefaultGameConfigDomain
+{
+    public static class DefaultGameConfig 
+    {
+        //use for contain config data 
+        public static float reloadTime = 2f;
+        public static float fireInterval = 0.5f;
+        public static int Bullet_Capacity = 30;
+        public static int BulletRemain_Capacity = 90;
+    
+    }
+}
+
